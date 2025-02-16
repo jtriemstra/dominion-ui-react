ea8bb0fee0b86ee897ea043413959a3499258746
joel <>
2025-02-16 14:04:14 -0500

    another domain change

--- src/Utility.js
+++ src/Utility.js
@@ -1,10 +1,10 @@
 export default class Utility {
     static apiServer() {
        // if (process.env.APP_ENV === 'prod'){
-       //     return "https://jtriemstra-dominion-api.herokuapp.com/v3";
+            return "https://jtriemstra-dominion-api.herokuapp.com/v3";
        // }
        // else {
-            return "http://localhost:8080/v3";
+            //return "http://localhost:8080/v3";
             //return "http://jtriemstradominionapi-env.eba-wcyvhkpu.us-east-2.elasticbeanstalk.com";
        // }
         
