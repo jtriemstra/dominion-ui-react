eb64867faea8ab637adf42bb7b802306c1fe501d
joel <>
2025-02-17 10:22:40 -0500

    reset master

--- README.md
+++
@@ -1,3 +0,0 @@
-# Dominion UI
-
-This is a companion UI to the services at https://github.com/jtriemstra/dominion-api. I'm using it as a way to learn some of the basics of React.
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import DominionUi from "./js/components/DominionUi";
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-
-
-const wrapper = document.getElementById("container");
-wrapper ? ReactDOM.render(<DominionUi  />, wrapper) : false;
