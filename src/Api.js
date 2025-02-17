5d965112f1ea34f9aaaa01d67a03e391f5ad4f3d
joel <>
2025-02-17 11:18:38 -0500

    env specific urls

--- src/Api.js
+++ src/Api.js
@@ -1,13 +1,6 @@
 export default class Api {
     apiServer() {
-        // if (process.env.APP_ENV === 'prod'){
-        //     return "https://jtriemstra-dominion-api.herokuapp.com/v3";
-        // }
-        // else {
-             return "http://localhost:8080/v3";
-             //return "http://jtriemstradominionapi-env.eba-wcyvhkpu.us-east-2.elasticbeanstalk.com";
-        // }
-         
+             return process.env.REACT_APP_API_ROOT;
      }
 
      fetchNull(url, callback) {
--- src/Utility.js
+++ src/Utility.js
@@ -1,13 +1,6 @@
 export default class Utility {
     static apiServer() {
-       // if (process.env.APP_ENV === 'prod'){
-       //     return "https://jtriemstra-dominion-api.herokuapp.com/v3";
-       // }
-       // else {
-            return "http://localhost:8080/v3";
-            //return "http://jtriemstradominionapi-env.eba-wcyvhkpu.us-east-2.elasticbeanstalk.com";
-       // }
-        
+            return process.env.REACT_APP_API_ROOT;
     }
 
     static disableNetwork() {
